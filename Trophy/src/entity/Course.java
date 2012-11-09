3ad4f80f43189ff20b4ed74f55a16d9dc9723b69
r0300423 <r0300423@herent.cs.kotnet.kuleuven.be.>
2012-11-09 15:06:56 +0100

    Course

--- Trophy/src/entity/Course.java
+++ Trophy/src/entity/Course.java
@@ -1,5 +1,19 @@
 package entity;
 
 public class Course {
-
+	private String coursename;
+	private int coursepoints;
+	
+	public Course(String cname, int cp) {
+		coursename = cname;
+		coursepoints = cp;
+	}
+	
+	public String getCourseName() {
+		return coursename;
+	}
+	
+	public int getCoursePoints() {
+		return coursepoints;
+	}
 }
