883ebc3e9c3aab0635d3cfecf07f5ba3c63d25a2
r0300423 <r0300423@herent.cs.kotnet.kuleuven.be.>
2012-11-09 15:15:35 +0100

    test

--- Trophy/src/database/CourseRegistry.java
+++ Trophy/src/database/CourseRegistry.java
@@ -1,9 +1,26 @@
 package database;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.http.*;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.FetchOptions;
 import entity.Course;
 
 public class CourseRegistry {
-	public Course getCourses() {
+	public boolean setCourse(Course c) {
+		Key activiteit = KeyFactory.createKey("Courses", "courses");
+
+		return false;
+	}
+	
+	public ArrayList<Course> getCourses() {
 		return null;
 	}
 }
