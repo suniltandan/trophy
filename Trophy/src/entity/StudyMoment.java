191cadd63499e9ba7cf59e0ca7b9a3008470cee9
VLStijn <stijn_vanlooveren@hotmail.com>
2012-11-09 17:01:40 +0100

    studymoment

--- Trophy/src/entity/StudyMoment.java
+++ Trophy/src/entity/StudyMoment.java
@@ -10,5 +10,46 @@ public class StudyMoment {
 	private Course course;
 	private int efficiency;
 	private StudyType studyType;
+	
+	public StudyMoment(Course c, Date startd, StudyType sp){
+	course = c;
+	startDate = startd;
+	studyType = sp;
+	}
+	
+	public void setStopDate(Date stopd){
+		stopDate = stopd;
+	}
+	
+	public void setEfficiency(int eff){
+		efficiency = eff;
+	}
+	
+	public Course getCourse(){
+		return course;
+	}
+	
+	public long getlength(){
+		long startlong = startDate.getTime();
+		long stoplong = stopDate.getTime();
+		long length=stoplong - startlong;
+		return length;
+	}
+	
+	public Date getStartDate(){
+		return startDate;
+	}
+	
+	public Date getStopDate(){
+		return stopDate;
+	}
+	
+	public StudyType getType(){
+		return studyType;
+	}
+	
+	public int getEfficiency(){
+		return efficiency;
+	}
 
 }
